35ba676aa9583fabe62226a73bb433928c56c0f2
Kostya Vasilyev <kmansoft@gmail.com>
2015-09-06 01:37:36 +0300

    initial checkin

---
+++ src/opt/android/datetimepicker/time/CenterHourView.java
@@ -0,0 +1,11 @@
+package opt.android.datetimepicker.time;
+
+import android.content.Context;
+import android.widget.TextView;
+
+public class CenterHourView extends TextView {
+
+	public CenterHourView(Context context) {
+		super(context);
+	}
+}
